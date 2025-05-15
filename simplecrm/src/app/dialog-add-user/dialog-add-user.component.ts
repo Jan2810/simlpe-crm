3c6e7cec632fbc79f8d34cf3d090fdde220d723f
Jan Steiner <jansteiner73@gmail.com>
2025-05-15 17:55:36 +0200

    dialog frame implemented

--- simplecrm/src/app/dialog-add-user/dialog-add-user.component.ts
+++ simplecrm/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, model, signal} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
+  readonly dialogRef = inject(MatDialogRef<DialogAddUserComponent>);
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- simplecrm/src/app/user/user.component.ts
+++ simplecrm/src/app/user/user.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddUserComponent } from './../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
@@ -21,4 +23,12 @@ import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 })
 export class UserComponent {
 
+  readonly dialog = inject(MatDialog);
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddUserComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
 }
