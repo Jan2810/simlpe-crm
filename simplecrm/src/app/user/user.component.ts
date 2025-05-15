14ae9f954a597063fa156672ab5d98184a920eab
Jan Steiner <jansteiner73@gmail.com>
2025-05-15 17:12:54 +0200

    changed tooltip position

--- simplecrm/src/app/user/user.component.ts
+++ simplecrm/src/app/user/user.component.ts
@@ -3,6 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -11,7 +12,9 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
-    MatTooltipModule
+    MatTooltipModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
