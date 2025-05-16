5bf57be7d4f9148de588d9dbf20c68f636bdff94
Jan Steiner <jansteiner73@gmail.com>
2025-05-16 08:48:09 +0200

    inputfields finished

--- simplecrm/src/app/dialog-add-user/dialog-add-user.component.ts
+++ simplecrm/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, inject, model, signal} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
@@ -6,12 +6,13 @@ import {
   MatDialog,
   MatDialogActions,
   MatDialogClose,
-  MatDialogContent,
   MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, provideNativeDateAdapter }   from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -22,10 +23,12 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule,
     MatButtonModule,
     MatDialogTitle,
-    MatDialogContent,
     MatDialogActions,
     MatDialogClose,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
