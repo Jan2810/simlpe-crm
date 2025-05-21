1a767f3c4840096e39ccb06c85d660e941bda147
Jan Steiner <jansteiner73@gmail.com>
2025-05-21 15:12:18 +0200

    progressbar for loading implemented

--- simplecrm/src/app/dialog-add-user/dialog-add-user.component.ts
+++ simplecrm/src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
@@ -14,12 +15,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../models/user.class';
-import { Firestore, collectionData, collection, doc, setDoc, addDoc } from '@angular/fire/firestore';;
+import { Firestore, collectionData, collection, doc, setDoc, addDoc } from '@angular/fire/firestore';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
   imports: [
+    CommonModule,
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
@@ -28,7 +31,8 @@ import { Firestore, collectionData, collection, doc, setDoc, addDoc } from '@ang
     MatDialogActions,
     MatDialogClose,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatProgressBarModule,
   ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-add-user.component.html',
@@ -36,6 +40,7 @@ import { Firestore, collectionData, collection, doc, setDoc, addDoc } from '@ang
 })
 export class DialogAddUserComponent {
   readonly dialogRef = inject(MatDialogRef<DialogAddUserComponent>);
+  isLoading = false;
   user: User = new User();
   firestore: Firestore = inject(Firestore);
   usersCol = collection(this.firestore, 'users');
@@ -48,8 +53,10 @@ export class DialogAddUserComponent {
 
   saveUser() {
     this.user.birthDate = new Date(this.user.birthDate).getTime();
+    this.isLoading = true;
     addDoc(this.usersCol, this.user.toJSON())
       .then(() => {
+        this.isLoading = false;
         console.log('this User added:', this.user);
       });
   }
