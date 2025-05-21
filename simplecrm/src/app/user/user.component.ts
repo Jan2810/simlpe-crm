b28358b9705030569ba8df7cec0825acdea55798
Jan Steiner <jansteiner73@gmail.com>
2025-05-21 22:12:23 +0200

    observable for users implemented

--- simplecrm/src/app/user/user.component.ts
+++ simplecrm/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
@@ -7,6 +7,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 import { DialogAddUserComponent } from './../dialog-add-user/dialog-add-user.component';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Firestore, collectionData, collection, doc, onSnapshot } from '@angular/fire/firestore';
+import { Observable, Subscription } from 'rxjs';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user',
@@ -19,13 +23,25 @@ import { DialogAddUserComponent } from './../dialog-add-user/dialog-add-user.com
     FormsModule,
     ReactiveFormsModule,
     MatCardModule,
+    CommonModule
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
-
+export class UserComponent implements OnInit {
+  firestore = inject(Firestore);
   readonly dialog = inject(MatDialog);
+  users$!: Observable<User[]>;
+  private sub!: Subscription;
+
+  ngOnInit(): void {
+    const usersCol = collection(this.firestore, 'users');
+    this.users$ = collectionData(usersCol, { idField: 'id' }) as Observable<User[]>;
+    this.sub = this.users$.subscribe(users => {
+      console.log('Users-Observable hat neue Daten:', users);
+    });
+  }
+
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogAddUserComponent);
 
