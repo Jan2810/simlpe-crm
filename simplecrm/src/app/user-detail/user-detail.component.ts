760afa9e28acb7da99ca80a55efb9f345133fe07
Jan Steiner <jansteiner73@gmail.com>
2025-06-15 19:04:09 +0200

    user detail component

--- simplecrm/src/app/user-detail/user-detail.component.ts
+++ simplecrm/src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
+
+
+
 export class UserDetailComponent {
+
+  userId: string = '';
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.userId = this.route.snapshot.params['id'];
+    console.log('aktuelle ID =', this.userId);
+  }
 
 }
