f1e8477c1a9aec841209be2c05ef6d4954949d9b
Jan Steiner <jansteiner73@gmail.com>
2025-06-15 19:39:09 +0200

    get user from firebase

--- simplecrm/src/app/user-detail/user-detail.component.ts
+++ simplecrm/src/app/user-detail/user-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
+import { Firestore, doc, collection, getDoc } from '@angular/fire/firestore';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
@@ -15,12 +17,29 @@ import { ActivatedRoute } from '@angular/router';
 export class UserDetailComponent {
 
   userId: string = '';
+  user: User = new User();
+  firestore = inject(Firestore);
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.userId = this.route.snapshot.params['id'];
     console.log('aktuelle ID =', this.userId);
+    this.getUser();
   }
 
+  getUser() {
+    const userDocRef = doc(this.firestore, 'users', this.userId);
+
+    getDoc(userDocRef).then((docSnap) => {
+      if (docSnap.exists()) {
+        this.user = new User(docSnap.data());
+        console.log('User gefunden:', this.user);
+      } else {
+        console.log('Kein User mit dieser ID gefunden.');
+      }
+    }).catch((error) => {
+      console.error('Fehler beim Abrufen des Users:', error);
+    });
+  }
 }
